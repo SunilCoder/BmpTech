f28f036c1903d5be901430784ea7dc34b282b80d
unknown <sunilthapa5898@gmail.com>
2020-07-07 17:14:38 +1000

    add users fragment with view model

---
+++ app/src/main/java/com/sunil/bmptech/fragments/UsersFragment.java
@@ -0,0 +1,38 @@
+package com.sunil.bmptech.fragments;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sunil.bmptech.R;
+
+public class UsersFragment extends Fragment {
+
+    private UsersViewModel mViewModel;
+
+    public static UsersFragment newInstance() {
+        return new UsersFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.users_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(UsersViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/sunil/bmptech/fragments/UsersViewModel.java
@@ -0,0 +1,7 @@
+package com.sunil.bmptech.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class UsersViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
